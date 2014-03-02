f813e3ddbe5ed929c6f2300e1a552321c39ea9c6
MathematicianVogt <MathematicianVogt@ryans-mbp-2.westell.com>
2014-03-02 18:02:36 -0500

    3

--- points.c
+++ points.c
@@ -174,19 +174,11 @@ struct Point furthestLeftPoint(struct Point a, struct Point z, struct Point poin
 }
 
 
-// Determine the set of points to the "left" of the line formed
-// from 'a' to 'z' (a->z).
-// Assumes numPoints is valid.
-// @param a Starting point of a line
-// @param z Ending point of a line
-// @param points A native array of Point's (immutable)
-// @param numPoints The number of valid Point's in points
-// @param lps A native array of Point's (set of Points to the left of a->z)
-// @return The number of points in the left point set
+
 int leftPointSet(struct Point a, struct Point z, struct Point points[], int numPoints, struct Point lps[])
 {
-	int numberOfPointsToTheLeft=0
-	int currentLpsele=0
+	int numberOfPointsToTheLeft=0;
+	int currentLpsele=0;
 	struct Point pivotX;
 	if(a.x<z.x)
 	{
@@ -205,7 +197,7 @@ int leftPointSet(struct Point a, struct Point z, struct Point points[], int numP
 	for(int i=0; i<numPoints; i++)
 	{
 
-		struct Point currentOne=points[i]
+		struct Point currentOne=points[i];
 		if(currentOne.x<pivotX.x)
 		{
 
@@ -233,26 +225,183 @@ int leftPointSet(struct Point a, struct Point z, struct Point points[], int numP
 // @param points A native array of Point's (immutable)
 // @param numPoints The number of valid Point's in points
 // @return the lowest Point
-struct Point lowestPoint(struct Point points[], int numPoints);
+struct Point lowestPoint(struct Point points[], int numPoints)
+{	
+
+	
+	struct Point goodY[numPoints];
+	int goodYEle=0;
+	struct Point smallestY=points[0];
+	for(int i=0; i<numPoints; i++)
+	{
+		struct Point currentStruc= points[i];
+
+		if(currentStruc.y<smallestY.y)
+		{
+			goodYEle=0;
+			memset(goodY, 0, sizeof(goodY));
+			smallestY=currentStruc;
+
+		}
+
+		if(currentStruc.y==smallestY.y)
+		{
+
+			goodY[goodYEle]=currentStruc;
+			goodY[goodYEle+1]=smallestY;
+			goodYEle=goodYEle+2;
+
+
+
+		}
+
+
+
+	}
+
+	if(goodY==NULL)
+	{
+
+
+		return smallestY;
+
+	}
+	else
+	{
+		struct Point smallestx=goodY[0];
+		for(int i =1; i<goodYEle; i++)
+		{
+			struct Point currentStruc=points[i];
+			if(currentStruc.x<smallestx.x)
+			{
+
+
+				smallestx=currentStruc;
+
+
+			}
+
+
+
+		}
+		return smallestx;
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+}
 
 
 int readPoints(struct Point points[])
-{
+{	char currentLabel;
+	int currentX;
+	int  currentY;
+	int pointsEle=0;
+	int counter=0;
+	int inputCounter=1;
 	char buff[MAX_LINE];
 	printf("Number of points(3-100):");
-	int numberOfPoints= getchar() - '0';
+	fgets(buff, MAX_LINE, stdin);
+	int numberOfPoints= atoi(buff);
+	printf("%d",numberOfPoints);
+	printf("%d",numberOfPoints<MIN_POINTS );
+	printf("%d",numberOfPoints);
+
 	
 
-	if((numberOfPoints<MIN_POINTS) || (numberOfPoints>MIN_POINTS))
+	if(numberOfPoints<MIN_POINTS || numberOfPoints>MAX_POINTS)
 	{
 		printf("Number of points must be between 3 and 100.\n");
 		return 0;
 
 	}
-	printf("Enter label (character):");
+	
+	while(counter<numberOfPoints)
+	{
+
+		if(inputCounter==1)
+		{
+
+			printf("Enter label (character): ");
+			if(fgets(buff, MAX_LINE, stdin))
+			{
+
+				currentLabel=buff[0];
+				inputCounter++;
+
+			}
+			else
+			{
+				printf("\nError reading point label.\n");
+				return 0;
+
+			}
+			
+		
+		}
+
+		else if(inputCounter==2)
+		{
+
+			printf("Enter x (int): ");
+			if(fgets(buff, MAX_LINE, stdin))
+			{
+			currentX=atoi(buff);
+			inputCounter++;
+			}
+			else
+			{
+				printf("\nError reading x coordinate.\n");
+				return 0;
+
+			}
 
 
-return 0;
+
+
+
+		}
+
+		else
+		{
+
+			printf("Enter y (int): ");
+			if(fgets(buff, MAX_LINE, stdin))
+			{
+				currentY=atoi(buff);
+			points[pointsEle].label=currentLabel;
+			points[pointsEle].x=currentX;
+			points[pointsEle].y=currentY;
+			pointsEle++;
+			inputCounter=1;
+			counter++;
+			}
+			else
+			{
+
+				printf("\nError reading y coordinate.\n");
+				return 0;
+
+
+
+			}
+
+		}
+		
+
+	}
+
+	printf("\n");
+	return numberOfPoints;
 
 
 
@@ -286,7 +435,24 @@ void swap(int a, int b, struct Point points[], int numPoints)
 
 int main( void ) {
 
-	printf("%d",MIN_POINTS);
+	struct Point myPoints[5];
+	struct Point test;
+	test.label='a';
+	test.x=6;
+	test.y=1;
+	struct Point test1;
+	test1.label='b';
+	test1.x=10;
+	test1.y=3;
+	struct Point lps[5];
+	int mypoints=readPoints(myPoints);
+	displayPoints(myPoints,mypoints);
+	printf("%d\n",indexOf(test,myPoints,mypoints));
+	displayPoint(leftmostPoint(myPoints,mypoints));
+	displayPoint(rightmostPoint(myPoints,mypoints));
+	displayPoint(furthestLeftPoint(test,test1,myPoints,mypoints));
+	int here=leftPointSet(test,test1,myPoints,mypoints,lps);
+	displayPoints(lps,here);
 	
 		return( 0 );
 }
