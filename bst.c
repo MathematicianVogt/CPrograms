79f0329af62821debcc923b56994495a14917aff
MathematicianVogt <MathematicianVogt@Ryans-MacBook-Pro-2.local>
2014-03-14 20:34:08 -0400

    Done

--- bst.c
+++ bst.c
@@ -1,3 +1,7 @@
+//@author Ryan Vogt
+//A Program that takes user input to make a binary search tree (slightly modiifed the tree can have multiple same values)
+//Then shows a post, pre and in order representation of the tree
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -24,43 +28,7 @@ typedef struct TreeNode {
 
 
 
-void cleanup_tree(TreeNode* root)
-{
 
-	if(*root==NULL)
-	{
-
-
-	}
-	else
-	{
-		postorder((*root)->left);
-		postorder((*root)->right);
-		free(root);
-
-
-	}
-
-
-
-
-}
-//a function that takes a root, a list of leaves, and the number of leaves
-//-root the root of the BST, assumed to be first element of list
-//-elements holds all elements that will be placed in the tree
-//-count, the number of elements in the list elements
-void build_tree(TreeNode** root, const int elements[], const int count)
-{
-
-	for(int i=0; i<count; i++)
-	{
-		insert(root,elements[i]);
-
-	}
-
-
-
-}
 //takes a root which is the tree, and goes through the tree until the proper insert can take place
 //root, the refrence to the overall root
 // x the variable that is to be added to the tree
@@ -85,12 +53,30 @@ void insert(TreeNode** root, const int x)
    (*root)->right=NULL;
 }
 
+//a function that takes a root, a list of leaves, and the number of leaves
+//-root the root of the BST, assumed to be first element of list
+//-elements holds all elements that will be placed in the tree
+//-count, the number of elements in the list elements
+void build_tree(TreeNode** root, const int elements[], const int count)
+{
+
+	for(int i=0; i<count; i++)
+	{
+		insert(root,elements[i]);
+
+	}
+
+
+
+}
+
+
 //takes a BST and prints it out using pre order, using recursion
-void preorder(TreeNode* root)
+void preorder(const TreeNode* root)
 {
 	
 
-	if(*root==NULL)
+	if(root==NULL)
 	{
 
 
@@ -103,10 +89,10 @@ void preorder(TreeNode* root)
 	}
 }
 //takes a BST starting with a root and prints it out using post order recursion
-void postorder(TreeNode* root)
+void postorder(const TreeNode* root)
 {
 
-	if(*root==NULL)
+	if(root==NULL)
 	{
 
 
@@ -115,7 +101,7 @@ void postorder(TreeNode* root)
 	{
 		postorder(root->left);
 		postorder(root->right);
-		printf("%d\n",(*root)->data );
+		printf("%d\n",root->data );
 
 
 	}
@@ -123,17 +109,17 @@ void postorder(TreeNode* root)
 }
 
 //takes a root of a BTS and prints it out in in-order with recursion
-void inorder(TreeNode* root)
+void inorder(const TreeNode* root)
 {
-	if(*root==NULL)
+	if(root==NULL)
 	{
 
 	}
 	else
 	{
-		preorder(root->left);
+		inorder(root->left);
 		printf("%d\n",root->data);
-		preorder(*root->right);
+		inorder(root->right);
 	}
 
 }
@@ -151,21 +137,21 @@ void traverse(const TreeNode* root, const TraversalType type)
 	if(type==PREORDER)
 	{
 		printf("Preorder:\n");
-		preorder(*root);
+		preorder(root);
 
 
 	}
 	if(type==INORDER)
 	{
 		printf("Inorder:\n");
-		inorder(*root);
+		inorder(root);
 
 
 	}
 	if(type==POSTORDER)
 	{
 		printf("Postorder:\n");
-		postorder(*root);
+		postorder(root);
 
 
 	}
@@ -173,10 +159,33 @@ void traverse(const TreeNode* root, const TraversalType type)
 
 
 }
+//takes a root of a tree and goes through the tree in post order in order to free the left, then right then root in order
+//to avoid memory leaks.
+void cleanup_tree(TreeNode* root)
+{
+
+	if(root==NULL)
+	{
+
+
+	}
+	else
+	{
+		cleanup_tree(root->left);
+		cleanup_tree(root->right);
+		free(root);
+
+
+	}
+
+
+
+
+}
 
 //main method to make a bst from input
 int main(int argc, char *argv[])
- {
+ {	//check if user sent input that is good to make a tree
  	if(argc<2)
  	{
 
@@ -188,6 +197,7 @@ int main(int argc, char *argv[])
  	}
  	else
  	{
+
  		if(atoi(argv[1])<=0)
  		{
 
@@ -196,13 +206,13 @@ int main(int argc, char *argv[])
  			return 0;
 
  		}
- 		
+ 		//if user sent good information go and create an array of inputs from user.
  		char buff[MAX_BUFF];
  		int numberOfInputs=atoi(argv[1]);
  		int nodeinputs[numberOfInputs];
  		int counter=0;
  		printf("Enter %d integer values to place in tree:\n", numberOfInputs);
-
+ 		//get inputs from the user
  		while(counter<numberOfInputs)
  		{
  			fgets(buff,MAX_BUFF, stdin);
@@ -210,7 +220,7 @@ int main(int argc, char *argv[])
  			counter++;
 
 		}
-		
+		//show the user inputs
 		printf("Input values:\n");	
 		for(int i=0; i<numberOfInputs; i++)
 		{
@@ -219,12 +229,13 @@ int main(int argc, char *argv[])
 
 
 		}
-
+		//build the tree and show the user the different representation of the trees
 		TreeNode* myRoot=NULL;
 		build_tree(&myRoot,nodeinputs,numberOfInputs);
 		traverse(myRoot,PREORDER);
 		traverse(myRoot,INORDER);
 		traverse(myRoot,POSTORDER);
+		cleanup_tree(myRoot);
 
 
 
