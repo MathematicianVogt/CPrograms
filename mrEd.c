356679aeeaf84c79700f885de943ed575dca47fe
MathematicianVogt <MathematicianVogt@Ryans-MacBook-Pro-2.local>
2014-04-17 12:18:31 -0400

    now done

--- mrEd.c
+++ mrEd.c
@@ -263,25 +263,21 @@
 			}
 			else if(strcmp(buff,"i")==0)
 			{	
-				printf("HIT\n");
+				
 		
 				int looping=1;
 				while(looping)
 				{
 					fgets(buff,MAX_LINE, stdin);
-					printf("HERE\n");
-					printf(" HERE DUDE %s\n",buff );
 					printf("%d\n",strcmp(buff,".") );
 					
 					if(strcmp(buff,".")==10)
 					{
-						printf("DONE\n");
 						break;
 
 					}
 					else
 					{
-						printf("THE DATA %s\n", buff );
 						dll_insert_at(lst,getCursorNumber(lst),(void *) buff);
 						dll_move_to(lst,getCursorNumber(lst));
 
@@ -307,7 +303,7 @@
 			}
 			else 
 			{
-				printf(" LOOK %s\n",buff );
+				
 			}
 }
 }
@@ -355,6 +351,240 @@
 	}	
 	void noExplictparam()
 	{
+		printf("no file supplied\n");
+		DlList_T lst=dll_create();
+		int hasChanged=0;
+		int looping=1;
+		char buff[MAX_LINE];
+		while(looping)
+ 		{
+ 			fgets(buff,MAX_LINE, stdin);
+ 			strtok(buff,"\n");
+			if(strcmp(buff,"Q")==0)
+			{
+
+				dll_destroy(lst);
+				break;
+
+
+
+			}
+			else if(strcmp(buff,".")==0)
+			{
+				showCursor(lst);
+
+			}
+
+			else if(strcmp(buff,"a")==0)
+			{
+
+				int currentlooping=1;
+				while(currentlooping)
+				{
+					fgets(buff,MAX_LINE, stdin);
+					strtok(buff,"\n");
+					if(strcmp(buff,".")==0)
+					{
+						
+						break;
+
+					}
+					else
+					{
+					
+						void* input=malloc(sizeof(char)*(strlen(buff)));
+						memcpy(input,buff,strlen(buff));
+						dll_append(lst, input);
+						dll_move_to(lst,dll_size(lst));
+						hasChanged=1;
+						//printf("SIZE %d\n",dll_size(lst) );
+						//showCursor(lst);
+						//printList(lst);
+
+					}
+
+
+
+				}
+
+
+
+			}
+			
+			else if(strcmp(buff, "\n")==0 || strcmp(buff,"+")==0)
+			{
+				if(getNext(lst)!=NULL)
+				{
+
+					dll_move_to(lst,getCursorNumber(lst) +1);
+
+
+
+				}
+				else
+				{
+					printf("?\n" );
+
+				}
+				
+
+			}
+			else if(strcmp(buff,"-")==0)
+			{
+
+				if(getPrevious(lst)!=NULL)
+				{
+
+					dll_move_to(lst,getCursorNumber(lst) -1);
+
+
+
+				}
+
+
+
+			}
+			else if(strcmp(buff,"$")==0)
+			{
+
+				if(getHead(lst)==NULL)
+				{
+
+					printf("?\n");
+
+				}
+				else
+				{
+
+				dll_move_to(lst,dll_size(lst));
+				showCursor(lst);
+				}
+
+			}
+			//NEEDS WORKS
+			else if(isdigit(buff))
+			{	printf("GOT HERE\n");
+				int newIndex=atoi(buff);
+				if(newIndex>=1 && newIndex<=dll_size(lst))
+				{
+
+					dll_move_to(lst,newIndex);
+
+				}
+
+
+
+			}
+			else if(strcmp(buff,".=")==0)
+			{
+
+
+				printf("%d\n",getCursorNumber(lst));
+
+			}
+			else if(strcmp(buff,"$=")==0)
+			{
+
+				printf("%d\n",dll_size(lst));
+
+
+			}
+			else if(strcmp(buff,"p")==0)
+			{
+				printListForward(lst);
+				dll_move_to(lst,dll_size(lst));
+
+
+			}
+			else if(strcmp(buff,"q")==0)
+			{
+
+				if(hasChanged)
+				{
+
+					printf("? buffer dirty\n");
+
+				}
+				else
+				{
+
+					dll_destroy(lst);
+					printf("\n");
+					printf("Bye\n");
+					break;
+
+
+
+				}
+
+
+
+			}
+			else if(strcmp(buff,"w")==0)
+			{
+
+
+				printf("?\n");
+
+
+			
+
+
+			}
+			else if(strcmp(buff,"wq")==0)
+			{
+					printf("?\n");
+
+
+
+
+
+				
+			}
+			else if(strcmp(buff,"i")==0)
+			{	
+				
+		
+				int looping=1;
+				while(looping)
+				{
+					fgets(buff,MAX_LINE, stdin);
+					printf("%d\n",strcmp(buff,".") );
+					
+					if(strcmp(buff,".")==10)
+					{
+						break;
+
+					}
+					else
+					{
+						dll_insert_at(lst,getCursorNumber(lst),(void *) buff);
+						dll_move_to(lst,getCursorNumber(lst));
+
+
+					}
+	
+
+				}
+				
+				
+	
+
+
+
+			}
+			else if(strcmp(buff,"d")==0)
+			{
+				dll_pop(lst,getCursorNumber(lst));
+
+
+
+			}
+			else 
+			{
+				
+			}
+}
 
 
 	}
